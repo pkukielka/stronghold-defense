7a5068f2408175a58fa82ded3b1eb58d4b152479
Piotr Kukielka <piotr.kukielka@gmail.com>
2013-06-07 19:09:10 +0200

    Change desktop app view dimensions

--- desktop/src/main/scala/Main.scala
+++ desktop/src/main/scala/Main.scala
@@ -5,8 +5,8 @@ import com.badlogic.gdx.backends.lwjgl._
 object Main extends App {
     val cfg = new LwjglApplicationConfiguration()
     cfg.title = "Stronghold Defense"
-    cfg.height = 800
-    cfg.width = 600
+    cfg.height = 600
+    cfg.width = 800
     cfg.useGL20 = true
     new LwjglApplication(new StrongholdDefense(), cfg)
 }
